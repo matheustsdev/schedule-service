f4720e2a330b6a0bc85ef6a54d200f548f6aa354
Matheus Teixeira <matheusts.dev@gmail.com>
2023-06-10 07:14:09 -0300

    refactor(frontend): removed html injection of all project

--- Frontend/data.js
+++
@@ -1,9 +0,0 @@
-let routeName = "forgotPassword"
-
-export function setRouteName(name) {
-    routeName = name
-}
-
-export function getRouteName() {
-    return routeName
-}
--- Frontend/routes.js
+++
@@ -1,4 +0,0 @@
-export const routes = {
-    signup: "./src/pages/signup/index.html",
-    forgotPassword: "./src/pages/forgotPassword/index.html",
-}
