e21d8292012c8a3efe97908405dfb515c2624e99
Livia Leite <livialeites1@gmail.com>
2023-06-13 20:10:54 -0300

    feat(frontend)

--- Frontend/src/pages/login/script.js
+++ Frontend/src/pages/login/script.js
@@ -5,13 +5,13 @@ const form = document.querySelector("form")
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    api.post("login/", {
+    api.post("auth", {
         data: {
             email: e.target[0].value,
             password: e.target[1].value
         }
     }).then((response) => {
-            window.location.replace(response.url)
+        response.window.location.replace('/Frontend/src/pages/mySchedulings')
     }).catch((error) => {
     console.log(error)
     })
