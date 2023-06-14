ec1715cc1917fb6154e0b071a69cafb840cfd3ea
Matheus Teixeira <matheusts.dev@gmail.com>
2023-06-13 23:17:30 -0300

    feature(frontend): added userData in localStorage

--- Frontend/src/pages/login/script.js
+++ Frontend/src/pages/login/script.js
@@ -1,4 +1,5 @@
 import { api } from "../../services/api.js"
+import jwtDecode from 'https://cdn.jsdelivr.net/npm/jwt-decode@3.1.2/+esm'
 
 const form = document.querySelector("form")
 
@@ -6,13 +7,17 @@ form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     api.post("auth", {
-        data: {
-            email: e.target[0].value,
-            password: e.target[1].value
-        }
-    }).then((response) => {
-       window.location.replace('/Frontend/src/pages/services')
-    }).catch((error) => {
+        email: e.target[0].value,
+        password: e.target[1].value
+    })
+    .then((response) => {
+        localStorage.setItem("@token", response.data.auth_token)
+        localStorage.setItem("@user", JSON.stringify(jwtDecode(response.data.jwt)))
+    })
+    .then(() => {
+        window.location.replace('/Frontend/src/pages/services')
+    })
+    .catch((error) => {
     console.log(error)
     })
 })
