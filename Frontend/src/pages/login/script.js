01934455a7f84fb131a6ed47c7733ed04b5f554d
Livia Leite <livialeites1@gmail.com>
2023-06-13 23:04:08 -0300

    feat(frontend): connection

---
+++ Frontend/src/pages/login/script.js
@@ -0,0 +1,18 @@
+import { api } from "../../services/api.js"
+
+const form = document.querySelector("form")
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    api.post("auth", {
+        data: {
+            email: e.target[0].value,
+            password: e.target[1].value
+        }
+    }).then((response) => {
+       window.location.replace('/Frontend/src/pages/services')
+    }).catch((error) => {
+    console.log(error)
+    })
+})
