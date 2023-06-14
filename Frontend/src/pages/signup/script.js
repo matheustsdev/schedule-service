a7b1d11858f682c8573edc4a5d73c0325078699a
Victor Quintanilla <victorgquintanilla@gmail.com>
2023-06-13 23:11:23 -0300

    feat (frontend): fixed signIn items

--- Frontend/src/pages/signup/script.js
+++ Frontend/src/pages/signup/script.js
@@ -6,13 +6,13 @@ form.addEventListener('submit', (e) => {
     e.preventDefault()
 
     api.post("signIn/", {
-        data: {
+         
             name: e.target[0].value,
             phone: e.target[1].value,
             email: e.target[2].value,
             password: e.target[3].value,
             confirmPassword: e.target[4].value,
-        }
+        
     }).then((response) => response.data.data window.location.href = "http://127.0.0.1:5500/Frontend/src/pages/scheduleService/scheduleService.html").catch((error) => {
         console.log(error)
     })
